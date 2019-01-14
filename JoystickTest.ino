4073e0634792bcf4d86b542be551316586a06ada
Cameron Husted <46616755+cehusted@users.noreply.github.com>
2019-01-14 16:20:16 -0500

    Small file to test joystick functionality

---
+++ JoystickTest.ino
@@ -0,0 +1,18 @@
+const int X_AXIS = 0;
+const int BUTTON = 7;
+int i;
+
+void setup() {
+  Serial.begin(9600);
+  digitalWrite(BUTTON, HIGH);
+  i = 0;
+}
+
+void loop() {
+  if (digitalRead(BUTTON) == LOW) {
+    i ++;
+    Serial.print("Button Pressed");
+    Serial.println(i);
+  }
+  delay(250);
+  }
